7a80e2950832a086c290c91b698979df155750d5
lmeeta <meeta9099@gmail.com>
2024-10-12 18:40:15 +0530

    Added some modification on css

--- src/Daily Practice/Home/Home.jsx
+++ src/Daily Practice/Home/Home.jsx
@@ -6,29 +6,52 @@ const Home = () => {
   return (
     <>
       <h1>Daily Activities</h1>
-      <ul>
-        <li>
-          Day 1: <Link to="/tableusingmap">Table Using Map</Link>
-        </li>
-        <li>
-          Day 2: <Link to="/carousel">Basic Carousel</Link>
-        </li>
-        <li>
-          Day 3: <Link to="/inputdatatotable">Input Data To Table</Link>
-        </li>
-        <li>
-          Day 4: <Link to="/propspassing">Props Passing Parent--Child</Link>
-        </li>
-        <li>
-          Day 5: <Link to="/basicReducer">Learning Basic Reducer</Link>
-        </li>
-        <li>
-          Day 6: <Link to="/filterdropdown">Filter Dropdown</Link>
-        </li>
-        <li>
-          Day 7: <Link to="/sortUsingDropdown">Sort Using Dropdown</Link>
-        </li>
-      </ul>
+      <div className="divStyle">
+        <ul className="ulStyle">
+          <li>
+            Day 1:
+            <Link to="/tableusingmap" className="linkStyling">
+              Table Using Map
+            </Link>
+          </li>
+          <li>
+            Day 2:
+            <Link to="/carousel" className="linkStyling">
+              Basic Carousel
+            </Link>
+          </li>
+          <li>
+            Day 3:
+            <Link to="/inputdatatotable" className="linkStyling">
+              Input Data To Table
+            </Link>
+          </li>
+          <li>
+            Day 4:
+            <Link to="/propspassing" className="linkStyling">
+              Props Passing Parent--Child
+            </Link>
+          </li>
+          <li>
+            Day 5:{" "}
+            <Link to="/basicReducer" className="linkStyling">
+              Learning Basic Reducer
+            </Link>
+          </li>
+          <li>
+            Day 6:{" "}
+            <Link to="/filterdropdown" className="linkStyling">
+              Filter Dropdown
+            </Link>
+          </li>
+          <li>
+            Day 7:{" "}
+            <Link to="/sortUsingDropdown" className="linkStyling">
+              Sort Using Dropdown
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
